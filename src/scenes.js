b081bf7bc08b691529d40d57e942629a5a875c3b
Wayne Myers <wgmyers@gmail.com>
2013-08-25 23:36:31 +0100

    More basic scenes added

--- src/scenes.js
+++ src/scenes.js
@@ -1,5 +1,32 @@
 // scenes.js
 
+// Scene for when the player has lost the game but doesn't know yet
+Crafty.scene('Losing', function() {
+
+    var c = 10
+
+    // Count down from ten to zero, taking a second each time
+    Crafty.e('2D, DOM, Text')
+       .text(c.toString())
+       .attr({ x: 0, y: Game.height() / 2, w: Game.width() })
+       .css($text_css);
+
+    
+
+
+});
+
+// Scene for when the player is playing the game
+Crafty.scene('Playing', function() {
+
+    // Actual game goes here
+
+    // If we screw up, we move to the losing scene
+    Crafty.scene('Losing');
+
+});
+
+
 // Start scene - display message and wait for key to be pressed
 Crafty.scene('Start', function() {
 
@@ -10,9 +37,22 @@ Crafty.scene('Start', function() {
 
     Crafty.e('2D, DOM, Text')
         .text('by Wayne Myers')
+        .attr({ x: 0, y: Game.height() / 2, w: Game.width() })
+        .css($text_css);
+
+    Crafty.e('2D, DOM, Text')
+        .text('Press any key to start')
         .attr({ x: 0, y: 2 * Game.height() / 3, w: Game.width() })
         .css($text_css);
 
+    this.start_game = function() {
+        Crafty.scene('Playing');
+    }
+    this.bind('KeyDown', this.start_game); 
+
+}, function() {
+    // Remove event binding once used
+    this.unbind('KeyDown', this.start_game);
 });
 
 
